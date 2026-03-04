db6b439c380645cf407a84a95f72f42f27241cde
sijun-yang <yangsijun5528@gmail.com>
2026-03-04 18:55:11 +0900

    주석 추가

--- common/db.py
+++ common/db.py
@@ -3,8 +3,11 @@ from pymongo.collection import Collection
 import os
 
 mongo_host = os.environ.get("MONGO_HOST", "localhost")
+
+# _를 붙여서 "db.이상한이름" 에 접근하는거 예방
 _client = MongoClient(f"mongodb://{mongo_host}:27017/")
 _db = _client["jungleqa"]
 
+# DB 접근 시 이걸로 고정해서 처리
 db_projects: Collection = _db["projects"]
 db_users: Collection = _db["users"]
--- common/dummy.py
+++ common/dummy.py
@@ -1,6 +1,7 @@
 from model.user import User
 
-"""현재 인증 된 사용자 정보를 불려오는 역할인데, 개발 도중에는 더미로 사용. 인증 파트 팀원이 구현 ㄱㄱ"""
+# 현재 인증 된 사용자 정보를 불려오는 역할인데, 개발 도중에는 더미로 사용. 인증 파트 팀원이 구현하기
+# 로그인이 안된 상태면 None을 반환하고, 아니면 더미 User 객체를 반환. 바꿔가면서 UI 테스트 ㄱㄱ
 def get_user_context(return_none: bool = False) -> User | None:
     if return_none:
         return None

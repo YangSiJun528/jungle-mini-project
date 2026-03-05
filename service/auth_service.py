41c271623a311f8259ceac9abfb1dab9e76544ac
이규정 <veracro97@gmail.com>
2026-03-05 19:50:16 +0900

    기존 중복 ID 구현방식 변경 => DB 측에서 login_id에 unique, 동시성 방지

--- service/auth_service.py
+++ service/auth_service.py
@@ -1,3 +1,5 @@
+from pymongo.errors import DuplicateKeyError
+
 from common.db import db_users
 from common.error import ServiceError
 from common import error
@@ -10,29 +12,32 @@ import jwt
 def auth_signup(
     username: str, user_id: str, password: str
 ) -> User | ServiceError:
-    # 1. 아이디 중복 체크
-    existing_user = db_users.find_one({"login_id": user_id})
-    if existing_user:
-        return error.DUPLICATE_ID_COMMON
 
-    # 2. 비밀번호 암호화
+    # 1. 비밀번호 암호화
     byted_password = password.encode('utf-8')
     hashed_password = bcrypt.hashpw(byted_password, bcrypt.gensalt())
     hashed_password_str = hashed_password.decode('utf-8')
 
-    # 3. DB 저장
-    result = db_users.insert_one({
-        "username": username,
-        "login_id": user_id,
-        "password_hash": hashed_password_str
-    })
+    try:
+        # 2. DB 저장
+        result = db_users.insert_one({
+            "username": username,
+            "login_id": user_id,
+            "password_hash": hashed_password_str
+        })
 
-    # 4. User 반환
-    return User(
-        username=username,
-        login_id=user_id,
-        password_hash=hashed_password_str,
-        _id=str(result.inserted_id))
+        # 3. User 반환
+        return User(
+            username=username,
+            login_id=user_id,
+            password_hash=hashed_password_str,
+            _id=str(result.inserted_id))
+
+        # 중복 ID에 대한 처리
+    except DuplicateKeyError:
+        return error.DUPLICATE_ID_COMMON
+    except Exception as e:
+        return error.UNKNOWN_ERROR_AT_SIGNUP_COMMON
 
 
 def auth_login(user_id: str, password: str) -> User | ServiceError:

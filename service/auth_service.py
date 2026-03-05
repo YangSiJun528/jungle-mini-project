5e42b51956aead0074de94c7f4fc485e52d4ba35
이규정 <veracro97@gmail.com>
2026-03-05 09:54:08 +0900

    사용자가 입력한 ID로 비밀번호 비교, JWT 토큰 생성 뼈대

--- service/auth_service.py
+++ service/auth_service.py
@@ -5,6 +5,7 @@ from model import User
 import bcrypt
 import jwt
 
+
 def auth_signup(username: str, user_id: str, password: str) -> User | ServiceError:
     # 1. 아이디 중복 체크
     existing_user = db_users.find_one({"login_id": user_id})
@@ -30,27 +31,33 @@ def auth_signup(username: str, user_id: str, password: str) -> User | ServiceErr
         password_hash=hashed_password_str,
         _id=str(result.inserted_id))
 
+
 def auth_login(user_id: str, password: str) -> User | ServiceError:
 
     # login_id로 유저 조회
     # 있으면 bcrypt로 비밀번호 검증
     id_exists = db_users.find_one({"login_id": user_id})
-    print("1")
+
     if not id_exists:
         return error.NOT_EXISTS_ID_COMMON
-    print("2")
+
     hashed_password = id_exists['password_hash']
     byted_hashed_password = hashed_password.encode('utf-8')
 
     is_password_match = bcrypt.checkpw(password.encode("utf-8"),
                                        byted_hashed_password)
-    
-    if is_password_match:
-        return print("로그인 성공")
-    # 성공이면 (user, token) 반환 => ?? 
-    # 
+    # 비밀번호가 일치하지 않으면 에러메시지
+    if not is_password_match:
+        return error.INVALID_CREDENTIALS_COMMON
+    # User 정보 반환 (password는 X)
+    return User(username=id_exists["username"],
+                login_id=id_exists["login_id"],
+                _id=str(id_exists["_id"]))
 
-    return ServiceError
+def create_access_token(user: User) -> str:
+    # 1) payload 만들기 (user_id, login_id, exp)
+    # 2) jwt.encode로 토큰 생성
+    return token
 
 def auth_get_user(user_id: str) -> User | ServiceError:
     return ServiceError
